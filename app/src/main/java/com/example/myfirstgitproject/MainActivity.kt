a627de69e1878c52b7a38418ce25bbe71b9ff73a
AlexManro <oleksandr.kyniuk@gmail.com>
2022-08-08 19:32:44 +0300

    New buttons

--- app/src/main/java/com/example/myfirstgitproject/MainActivity.kt
+++ app/src/main/java/com/example/myfirstgitproject/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.myfirstgitproject
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.example.myfirstgitproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bindingClass : ActivityMainBinding
+    val a = 324
+    val b = 34
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
+        bindingClass.b1.setOnClickListener {
+
+            val result = a + b
+            bindingClass.tvResult.text = "Результат сложения : $result"
+        }
+        bindingClass.b2.setOnClickListener {
+            val result = a - b
+            bindingClass.tvResult.text = "Результат сложения : $result"
+        }
+        bindingClass.b3.setOnClickListener {
+            val result = a * b
+            bindingClass.tvResult.text = "Результат сложения : $result"
+
+        }
+
+
+
     }
+
 }
+
